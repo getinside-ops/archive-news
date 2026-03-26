7d0442559ed3ae55d52613db223cf03f52c63a75
benoit-prentout <prentoub@gmail.com>
2026-03-26 21:38:02 +0100

    fix: recover link data and change empty-pixel score from FAIL to WARN

--- apply_changes.py
+++ apply_changes.py
@@ -79,6 +79,8 @@ def apply_changes():
         metadata['preheader'] = parser.preheader
         metadata['crm'] = crm
         metadata['audit'] = parser.audit
+        metadata['links'] = parser.links
+        metadata['detected_pixels'] = parser.detected_pixels
         
         # Decode sender if it's MIMED
         sender = metadata.get('sender', 'Unknown')
